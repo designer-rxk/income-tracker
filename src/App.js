dbeede7a473638a534b28121f2040eddfaf5519b
Raix Karks <raix.karks98@gmail.com>
2022-03-17 17:57:46 +0200

    Added localStorage and fixed total amount display.

--- src/App.js
+++ src/App.js
@@ -9,12 +9,28 @@ function App() {
 	const [totalIncome, setTotalIncome] = useState(0);
 
 	useEffect(() => {
+	const getLocalIncome = () => {
+	  if(localStorage.getItem("income") === null) {
+		localStorage.setItem("income", JSON.stringify([]));
+	  } else {
+		let incomeLocal = JSON.parse(localStorage.getItem("income"))
+		setIncome(incomeLocal);
+	  }
+	}
+	getLocalIncome();
+	}, []);
+	const saveLocalIncome = () => {
+	localStorage.setItem("income", JSON.stringify(income))
+	};
+	useEffect(() => {
 		let temp = 0;
 		for(let i = 0; i < income.length; i++) {
 			temp += parseFloat(income[i].price);
 		}
 		setTotalIncome(temp);
+		saveLocalIncome();
 	}, [income]);
+
 	return (
 		<div className="App">
 			<Header totalIncome={totalIncome} />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ function Header({totalIncome}){
     return(
         <header>
             <h1>Income Tracker</h1>
-            <div className={"total-income"}>Total: {totalIncome}€</div>
+            <div className={"total-income"}>Total: {totalIncome.toFixed(2)}€</div>
         </header>
     );
 }
